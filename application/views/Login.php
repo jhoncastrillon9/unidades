3428583bb637f8253cc6cbe9705fac3f880abfa5
Jhon <jhoncastrillon9@gmail.com>
2018-08-21 22:15:21 -0500

    Configuraciones

---
+++ application/views/Login.php
@@ -0,0 +1,58 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>InmobiliariaApp</title>
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">	
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</head>
+<body>
+
+<div class="container-login row col-sm-12">
+	<div class="Container-Img col-sm-8">
+		<div class="container-button row align-items-start">
+			<div class="btn-ayuda">
+				Solicitar Ayuda
+			</div>
+		</div>
+		<div class="container-text row align-items-end">
+			<div class="">¡Ayudamos a las <strong>Inmobiliarias</strong></div>
+			<div>en la administración de las cuentas FAcilmente!</div>
+		</div>
+	</div>
+	<div class="container-form">
+	
+			<div class="container-logo">
+				<img src="" alt="">
+			</div>
+
+			<div class="container-input">
+				<form>
+					<div class="form-group">
+						<label for="formGroupExampleInput">Example label</label>
+						<input type="text" class="form-control" id="formGroupExampleInput" placeholder="Example input">
+					</div>
+					<div class="form-group">
+						<label for="formGroupExampleInput2">Another label</label>
+						<input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Another input">
+					</div>
+					<div class="container-buttons">
+						<button type="submit">Entrar</button>
+					</div>
+					<div class="container-registrer">
+						¿No tienes cuenta? Registrate Aquí
+					</div>
+				</form>				
+			</div>
+		<?php echo base_url()?>
+	</div>
+</div>
+<?php include 'shared/Footer.php'; ?>
+
+</body>
+</html>
---
+++ application/views/Shared/Footer.php
@@ -0,0 +1,18 @@
+<footer>
+	<div class="container-footer">
+		<div class="footer1 col-sm-12 row justify-content-md-center">
+			<div>
+				<img src="" alt="">
+				Si tienes alguna inquietud, Llámanos al 301 609 86 45
+			</div>
+		</div>
+		<div class="footer2 col-sm-12 row justify-content-md-center">
+			<div>Inmobiliariapp</div>
+			<div>Preguntas Frecuentes</div>
+			<div>Terminos y Condiciones</div>
+			<div>Soporte</div>
+			<div>Ayuda</div>
+		</div>
+	</div>
+
+</footer>
